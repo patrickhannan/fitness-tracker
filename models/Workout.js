1c13154304e104cb318345aff2f1d589eda7dd7f
Patrick Hannan <patrick@hannan.com>
2020-10-23 23:30:27 -0400

    Added workout schema

--- models/Workout.js
+++ models/Workout.js
@@ -1,2 +1,34 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema(
+    {
+      day: {
+        type: Date,
+        default: Date.now,
+      },
+      exercises: [
+        {
+          type: {
+            type: String,
+            trim: true,
+          },
+          name: {
+            type: String,
+            trim: true,
+          },
+          duration: Number,
+          distance: Number,
+          weight: Number,
+          reps: Number,
+          sets: Number,
+        },
+      ],
+    },
+    {
+      toJSON: {
+        virtuals: true,
+      },
+    }
+  );
+  
