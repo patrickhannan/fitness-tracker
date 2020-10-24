dbaebf4d1510c6f20754a4fa0e95b190152b9c92
Patrick Hannan <patrick@hannan.com>
2020-10-23 23:31:56 -0400

    Export workout model.

--- models/Workout.js
+++ models/Workout.js
@@ -31,4 +31,13 @@ const workoutSchema = new Schema(
       },
     }
   );
+
+workoutSchema.virtual("totalDuration").get(function () {
+    return this.exercises.reduce((total, exercises) => {
+        return total + exercises.duration;
+    }, 0)
+});
   
+const Workout = mongoose.model("Workout", workoutSchema);
+  
+module.exports = Workout;
