b8020e54b04fbdbd21c3045b6b991c23e237d5c2
Patrick Hannan <patrick@hannan.com>
2020-10-24 00:23:25 -0400

    Added api routes.

--- server.js
+++ server.js
@@ -20,15 +20,65 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
 // View routes
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "./public/index.html"));
-  });
+});
   
-  app.get("/exercise", (req, res) => {
+app.get("/exercise", (req, res) => {
     res.sendFile(path.join(__dirname, "./public/exercise.html"));
-  });
+});
   
-  app.get("/stats", (req, res) => {
+app.get("/stats", (req, res) => {
     res.sendFile(path.join(__dirname, "./public/stats.html"));
+});
+
+// Api routes
+app.get("/api/workouts", function (req, res) {
+    db.Workout.find({})
+      .then((data) => {
+        res.json(data);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.json({
+          error: true,
+          data: null,
+          message: "Can not find workouts.",
+        });
+      });
   });
+
+app.post("/api/workouts", (req, res) => {
+    db.Workout.create({})
+      .then((data) => {
+        res.json(data);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.json({
+          error: true,
+          data: null,
+          message: "Failed to create workout.",
+        });
+      });
+});
+
+app.put("/api/workouts/:id", (req, res) => {
+    db.Workout.findByIdAndUpdate(
+      req.params.id,
+      { $push: { exercises: req.body } },
+      { new: true }
+    )
+      .then((data) => {
+        res.json(data);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.json({
+          error: true,
+          data: null,
+          message: "Failed to update workout.",
+        });
+      });
+});
 
 // Connection
 const connection = mongoose.connection;
